f93c822788227918f720c46e7c3e955588836e6e
SpencerPulliam <spencer_pulliam@yahoo.com>
2019-02-21 09:29:23 -0600

    empty maven project

---
+++ ers/src/main/java/pulliam-p1/App.java
@@ -0,0 +1,13 @@
+package pulliam-p1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
