86989fb838a5235567e0be3ab2fb07cc4249f87c
SpencerPulliam <spencer_pulliam@yahoo.com>
2019-02-21 11:22:48 -0600

    Basic scanner added.

---
+++ ers/src/main/java/pulliamP1/Main.java
@@ -0,0 +1,14 @@
+package pulliamP1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main( String[] args ){
+        
+        Scanner sc = new Scanner(System.in);
+        String out = sc.next();
+        System.out.println(out);
+        sc.close();
+    }
+}
+
